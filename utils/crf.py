0456daad6c767863c8fbe2a82d08b2095dbeed3f
Bruno Artacho <44781803+bmartacho@users.noreply.github.com>
2019-12-05 14:45:16 -0500

    Add files via upload

--- utils/crf.py
+++ utils/crf.py
@@ -1,1 +1,79 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
+import os
+import sys
+
+import numpy as np
+import imageio
+import argparse
+import logging
+import time
+
+import pyximport
+pyximport.install()
+
+import utils.convcrf as convcrf
+import torch
+from torch.autograd import Variable
+
+from utils import pascal_visualizer as vis
+from utils import synthetic
+
+import matplotlib.pyplot as plt
+
+import pydensecrf.densecrf as dcrf
+import pydensecrf.utils as utils
+import numpy as np
+
+
+def crf_inference(image, unary):
+
+    # get basic hyperparameters
+    num_classes = unary.shape[0]
+    shape       = image.shape[1:3]
+    config      = convcrf.default_conf
+
+    config['filter_size'] = 7
+    config['pyinn']       = False
+
+    image     = image.reshape([1, 3, shape[0], shape[1]])
+
+    unary     = unary.reshape([1, num_classes, shape[0], shape[1]])
+
+    gausscrf = convcrf.GaussCRF(conf=config, shape=shape, nclasses=num_classes)
+
+    gausscrf.cuda()
+
+    prediction = gausscrf.forward(unary=unary, img=image)
+
+    return prediction
+
+
+class DenseCRF(object):
+    def __init__(self,iter_max,pos_w,pos_xy_std,bi_w,bi_xy_std,bi_rgb_std):
+        self.iter_max   = iter_max
+        self.pos_w      = pos_w
+        self.pos_xy_std = pos_xy_std
+        self.bi_w       = bi_w
+        self.bi_xy_std  = bi_xy_std
+        self.bi_rgb_std = bi_rgb_std
+        
+    def __call__(self,image,probmap):
+        C,H,W = probmap.shape
+        
+        U = utils.unary_from_softmax(probmap)
+        U = np.ascontiguousarray(U)
+        
+        image = np.ascontiguousarray(image)
+        
+        d = dcrf.DenseCRF2D(W,H,C)
+        d.setUnaryEnergy(U)
+        d.addPairwiseGaussian(sxy=self.pos_xy_std,compat=self.pos_w)
+        d.addPairwiseBilateral(sxy=self.bi_xy_std,srgb=self.bi_rgb_std,rgbim=image,compat=self.bi_w)
+        
+        Q = d.inference(self.iter_max)
+        Q = np.array(Q).reshape((C,H,W))
+        
+        return Q
---
+++ utils/loss.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class SegmentationLosses(object):
+    def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False):
+        self.ignore_index = ignore_index
+        self.weight = weight
+        self.size_average = size_average
+        self.batch_average = batch_average
+        self.cuda = cuda
+
+    def build_loss(self, mode='ce'):
+        """Choices: ['ce' or 'focal']"""
+        if mode == 'ce':
+            return self.CrossEntropyLoss
+        elif mode == 'focal':
+            return self.FocalLoss
+        else:
+            raise NotImplementedError
+
+    def CrossEntropyLoss(self, logit, target):
+        n, c, h, w = logit.size()
+        criterion = nn.CrossEntropyLoss(weight=self.weight, ignore_index=self.ignore_index,
+                                        size_average=self.size_average)
+        if self.cuda:
+            criterion = criterion.cuda()
+
+        loss = criterion(logit, target.long())
+
+        if self.batch_average:
+            loss /= n
+
+        return loss
+
+    def FocalLoss(self, logit, target, gamma=2, alpha=0.5):
+        n, c, h, w = logit.size()
+        criterion = nn.CrossEntropyLoss(weight=self.weight, ignore_index=self.ignore_index,
+                                        size_average=self.size_average)
+        if self.cuda:
+            criterion = criterion.cuda()
+
+        logpt = -criterion(logit, target.long())
+        pt = torch.exp(logpt)
+        if alpha is not None:
+            logpt *= alpha
+        loss = -((1 - pt) ** gamma) * logpt
+
+        if self.batch_average:
+            loss /= n
+
+        return loss
+        
+class CrossEntropymsc2d(nn.Module):
+
+    def __init__(self, size_average=True, ignore_label=255):
+        super(CrossEntropymsc2d, self).__init__()
+        self.size_average = size_average
+        self.ignore_label = ignore_label
+
+    def forward(self, predict, target, weight=None):
+        """
+            Args:
+                predict:(n, c, h, w)
+                target:(n, h, w)
+                weight (Tensor, optional): a manual rescaling weight given to each class.
+                                           If given, has to be a Tensor of size "nclasses"
+        """
+        assert not target.requires_grad
+        assert predict.dim() == 4
+        assert target.dim() == 3
+        assert predict.size(0) == target.size(0), "{0} vs {1} ".format(predict.size(0), target.size(0))
+        assert predict.size(2) == target.size(1), "{0} vs {1} ".format(predict.size(2), target.size(1))
+        assert predict.size(3) == target.size(2), "{0} vs {1} ".format(predict.size(3), target.size(3))
+        n, c, h, w = predict.size()
+        target_mask = (target >= 0) * (target != self.ignore_label)
+        target = target[target_mask]
+        if not target.data.dim():
+            return Variable(torch.zeros(1))
+        predict = predict.transpose(1, 2).transpose(2, 3).contiguous()
+        predict = predict[target_mask.view(n, h, w, 1).repeat(1, 1, 1, c)].view(-1, c)
+        loss = SegmentationLosses.CrossEntropyLoss(predict, target)
+        return loss
+
+if __name__ == "__main__":
+    loss = SegmentationLosses(cuda=True)
+    a = torch.rand(1, 3, 7, 7).cuda()
+    b = torch.rand(1, 7, 7).cuda()
+    print(loss.CrossEntropyLoss(a, b).item())
+    print(loss.FocalLoss(a, b, gamma=0, alpha=None).item())
+    print(loss.FocalLoss(a, b, gamma=2, alpha=0.5).item())
+
+
+
+
